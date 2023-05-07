89f8f6351dd0e1b54854dd7d170810ddc23137f5
Davi Hasson Castro <davihasson@gmail.com>
2023-05-07 18:56:02 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "overrides": [
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "windows"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "jsx-quotes": [
+      2,
+      "prefer-double"
+    ],
+    "eol-last": [
+      "error",
+      "always"
+    ]
+  }
+};
